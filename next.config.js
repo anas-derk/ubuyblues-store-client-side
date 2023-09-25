3439395d9a4256982dd49b5bcd1cfc38cc5bd4dc
anas derk <anas.derk2020@gmail.com>
2023-09-25 08:40:15 -0700

    Initial The Project

--- next.config.js
+++ next.config.js
@@ -1,6 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  env: {
+    BASE_API_URL: process.env.NODE_ENV === "development" ? "http://localhost:5300" : "https://api.asfourintlco.com",
+  },
+  async headers() {
+    return [
+      {
+        source: process.env.NODE_ENV === "development" ? "//localhost:5300/(.*)" : "//api.asfourintlco.com/(.*)",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://asfourintlco.com",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ]
+      }
+    ];
+  }
 }
 
 module.exports = nextConfig
