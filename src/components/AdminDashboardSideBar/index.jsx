c2d76c2617ef542352b53f2590d7de7f1d4cd8c3
anas derk <anas.derk2020@gmail.com>
2023-10-04 09:45:40 -0700

    Continue With Design Admin Dashboard Page

--- src/components/AdminDashboardSideBar/index.jsx
+++ src/components/AdminDashboardSideBar/index.jsx
@@ -1,13 +1,19 @@
 import Logo from "../../../public/images/Logo-ASFOUR-White-footer.png";
+import { MdProductionQuantityLimits } from "react-icons/md";
+import Link from "next/link";
 
 export default function AdminDashboardSideBar() {
     return (
         <aside className="admin-dashboard-side-bar pt-3">
             <img src={Logo.src} alt="logo" width="100" height="100" className="d-block mx-auto logo-img" />
             <hr className="mb-0" />
-            <ul className="managment-links-list">
-                <li className="p-3 managment-link">Products</li>
-                <li className="p-3 managment-link">Orders</li>
+            <ul className="managment-items-list">
+                <li className="managment-item p-3">
+                    <MdProductionQuantityLimits className="me-2" />
+                    <Link href="/admin-dashboard" className="managment-link">
+                        Products
+                    </Link>
+                </li>
             </ul>
         </aside>
     );
--- src/pages/admin-dashboard/index.jsx
+++ src/pages/admin-dashboard/index.jsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import AdminDashboardSideBar from "@/components/AdminDashboardSideBar";
+import { PiHandWavingThin } from "react-icons/pi";
 
 export default function AdminDashboard() {
     return (
@@ -7,8 +8,12 @@ export default function AdminDashboard() {
             <Head>
                 <title>Asfour Store - Admin Dashboard</title>
             </Head>
-            <div className="page-content">
-                <AdminDashboardSideBar />
+            <AdminDashboardSideBar />
+            <div className="page-content d-flex justify-content-center align-items-center">
+                <h1 className="fw-bold w-fit pb-2">
+                    <PiHandWavingThin className="me-2" />
+                    Hi, Mr Asfour In Your Admin Dashboard Page
+                </h1>
             </div>
         </div>
     );
