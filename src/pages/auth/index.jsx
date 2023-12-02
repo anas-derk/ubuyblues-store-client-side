48495649bc24b5781109c22184f49c19d23cab21
anas derk <anas.derk2020@gmail.com>
2023-12-02 09:45:51 -0800

    Make Cart + Auth Pages Responsive

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -202,26 +202,26 @@ export default function UserLogin() {
                         <section className="authentication">
                             <div className="row">
                                 {appearedAuthPartName === "sign-up" && <div className="col-md-12">
-                                    <div className="signup-section ps-5">
+                                    <div className="signup-section">
                                         <h3 className="part-name mb-4">Create New Account</h3>
                                         <form className="user-signup-form mb-3" onSubmit={userSignup}>
                                             <div className="email-field-box">
                                                 <input
                                                     type="text"
                                                     placeholder="Please Enter Your Email"
-                                                    className={`form-control p-3 border-2 ${formValidationErrors["emailForSignup"] ? "border-danger mb-2" : "mb-5"}`}
+                                                    className={`form-control p-3 border-2 ${formValidationErrors["emailForSignup"] ? "border-danger mb-3" : "mb-5"}`}
                                                     onChange={(e) => setEmailForSignup(e.target.value.trim())}
                                                 />
                                                 <div className='icon-box text-dark'>
                                                     <BiSolidUser className="icon" />
                                                 </div>
                                             </div>
-                                            {formValidationErrors["emailForSignup"] && <p className='error-msg text-danger'>{formValidationErrors["emailForSignup"]}</p>}
+                                            {formValidationErrors["emailForSignup"] && <p className='error-msg text-white bg-danger p-2 mb-4'>{formValidationErrors["emailForSignup"]}</p>}
                                             <div className="password-field-box">
                                                 <input
                                                     type={isVisiblePasswordForSignup ? "text" : "password"}
                                                     placeholder="Please Enter Your Password"
-                                                    className={`form-control p-3 border-2 ${formValidationErrors["passwordForSignup"] ? "border-danger mb-2" : "mb-5"}`}
+                                                    className={`form-control p-3 border-2 ${formValidationErrors["passwordForSignup"] ? "border-danger mb-3" : "mb-5"}`}
                                                     onChange={(e) => setPasswordForSignup(e.target.value.trim())}
                                                 />
                                                 <div className='icon-box text-dark'>
@@ -229,38 +229,38 @@ export default function UserLogin() {
                                                     {isVisiblePasswordForSignup && <AiOutlineEyeInvisible className='invisible-eye-icon icon' onClick={() => setIsVisiblePasswordForSignup(value => value = !value)} />}
                                                 </div>
                                             </div>
-                                            {formValidationErrors["passwordForSignup"] && <p className='error-msg text-danger'>{formValidationErrors["passwordForSignup"]}</p>}
+                                            {formValidationErrors["passwordForSignup"] && <p className='error-msg text-white bg-danger p-2 mb-4'>{formValidationErrors["passwordForSignup"]}</p>}
                                             {!isLoginingStatus && !errMsg && <button type="submit" className="btn btn-success w-100 mb-4 p-3">
                                                 <span className="me-2">Signup</span>
                                                 <FiLogIn />
                                             </button>}
-                                            {isLoginingStatus && <button disabled className="btn btn-primary w-100 mb-4">
+                                            {isLoginingStatus && <button disabled className="btn btn-primary w-100 mb-4 p-3">
                                                 <span className="me-2">Wait Signup ...</span>
                                             </button>}
                                         </form>
                                     </div>
                                 </div>}
                                 {appearedAuthPartName === "login" && <div className="col-md-12">
-                                    <div className="login-section ps-5">
+                                    <div className="login-section">
                                         <h3 className="part-name mb-4">Login</h3>
                                         <form className="user-login-form mb-3" onSubmit={userLogin}>
                                             <div className="email-field-box">
                                                 <input
                                                     type="text"
                                                     placeholder="Please Enter Your Email"
-                                                    className={`form-control p-3 border-2 ${formValidationErrors["emailForLogin"] ? "border-danger mb-2" : "mb-5"}`}
+                                                    className={`form-control p-3 border-2 ${formValidationErrors["emailForLogin"] ? "border-danger mb-4" : "mb-5"}`}
                                                     onChange={(e) => setEmailForLogin(e.target.value.trim())}
                                                 />
                                                 <div className='icon-box text-dark'>
                                                     <BiSolidUser className="icon" />
                                                 </div>
                                             </div>
-                                            {formValidationErrors["emailForLogin"] && <p className='error-msg text-danger'>{formValidationErrors["emailForLogin"]}</p>}
+                                            {formValidationErrors["emailForLogin"] && <p className='error-msg text-white bg-danger p-2 mb-4'>{formValidationErrors["emailForLogin"]}</p>}
                                             <div className="password-field-box">
                                                 <input
                                                     type={isVisiblePasswordForLogin ? "text" : "password"}
                                                     placeholder="Please Enter Your Password"
-                                                    className={`form-control p-3 border-2 ${formValidationErrors["passwordForLogin"] ? "border-danger mb-2" : "mb-5"}`}
+                                                    className={`form-control p-3 border-2 ${formValidationErrors["passwordForLogin"] ? "border-danger mb-4" : "mb-5"}`}
                                                     onChange={(e) => setPasswordForLogin(e.target.value.trim())}
                                                 />
                                                 <div className='icon-box text-dark'>
@@ -268,12 +268,12 @@ export default function UserLogin() {
                                                     {isVisiblePasswordForLogin && <AiOutlineEyeInvisible className='invisible-eye-icon icon' onClick={() => setIsVisiblePasswordForLogin(value => value = !value)} />}
                                                 </div>
                                             </div>
-                                            {formValidationErrors["passwordForLogin"] && <p className='error-msg text-danger'>{formValidationErrors["passwordForLogin"]}</p>}
+                                            {formValidationErrors["passwordForLogin"] && <p className='error-msg text-white bg-danger p-2 mb-4'>{formValidationErrors["passwordForLogin"]}</p>}
                                             {!isLoginingStatus && !errMsg && <button type="submit" className="btn btn-success w-100 mb-4 p-3">
                                                 <span className="me-2">Login</span>
                                                 <FiLogIn />
                                             </button>}
-                                            {isLoginingStatus && <button disabled className="btn btn-primary w-100 mb-4">
+                                            {isLoginingStatus && <button disabled className="btn btn-primary w-100 mb-4 p-3">
                                                 <span className="me-2">Wait Loging ...</span>
                                             </button>}
                                         </form>
--- src/pages/cart/index.jsx
+++ src/pages/cart/index.jsx
@@ -1,9 +1,10 @@
 import Head from "next/head";
-import { useState, useEffect } from "react";
+import { useState, useEffect, Fragment } from "react";
 import Header from "@/components/Header";
 import { HiMinus, HiPlus } from "react-icons/hi";
 import { BsTrash } from "react-icons/bs";
 import Link from "next/link";
+import { PiSmileySad } from "react-icons/pi";
 
 export default function Cart() {
     const [allProductsData, setAllProductsData] = useState([]);
@@ -15,6 +16,7 @@ export default function Cart() {
     const [errMsg, setErrorMsg] = useState("");
     const [successMsg, setSuccessMsg] = useState("");
     const [isWaitStatus, setIsWaitStatus] = useState(false);
+    const [windowInnerWidth, setWindowInnerWidth] = useState(0);
     useEffect(() => {
         let allProductsData = JSON.parse(localStorage.getItem("asfour-store-user-cart"));
         if (Array.isArray(allProductsData)) {
@@ -28,6 +30,18 @@ export default function Cart() {
                     totalPriceAfterDiscount,
                 });
                 setAllProductsData(allProductsData);
+                setWindowInnerWidth(window.innerWidth);
+                window.addEventListener("resize", () => {
+                    setWindowInnerWidth(window.innerWidth);
+                });
+                window.addEventListener("scroll", function (){
+                    if (this.innerWidth < 991) {
+                        let cartTotalBtnBox = document.querySelector(".products .cart-total-btn-box");
+                        if (this.scrollY < 613) {
+                            cartTotalBtnBox.style.display = "block";
+                        } else cartTotalBtnBox.style.display = "none";
+                    }
+                });
             }
         }
     }, []);
@@ -102,8 +116,8 @@ export default function Cart() {
                 <div className="container-fluid">
                     {(errMsg || successMsg) && <p className={`result-update-cart-msg text-white text-start mb-5 p-3 alert ${errMsg ? "alert-danger bg-danger" : ""} ${successMsg ? "alert-success bg-success" : ""}`}>{errMsg || successMsg}</p>}
                     {allProductsData.length > 0 ? <div className="row align-items-center">
-                        <div className="col-md-8">
-                            <section className="products w-100">
+                        <div className="col-xl-8">
+                            {windowInnerWidth > 991 && <section className="products w-100">
                                 <table className="user-products-table mb-4 w-100 text-start">
                                     <thead>
                                         <tr>
@@ -118,10 +132,10 @@ export default function Cart() {
                                             <tr key={product.id}>
                                                 <td className="product-cell">
                                                     <div className="row">
-                                                        <div className="col-md-3">
+                                                        <div className="col-lg-4">
                                                             <img src={`${process.env.BASE_API_URL}/${product.imagePath}`} width="100" height="100" />
                                                         </div>
-                                                        <div className="col-md-9">
+                                                        <div className="col-lg-8">
                                                             <h5 className="product-name mb-3">{product.name}</h5>
                                                             <h6 className={`product-price ${product.discount != 0 ? "text-decoration-line-through" : ""}`}>{product.price} $</h6>
                                                             {product.discount != 0 && <h6 className="product-after-discount">{product.price - product.discount} $</h6>}
@@ -139,7 +153,7 @@ export default function Cart() {
                                                         />
                                                     </div>
                                                 </td>
-                                                <td>
+                                                <td className="subtotal-cell">
                                                     {product.price * product.quantity} $
                                                 </td>
                                                 <td className="delete-product-cell">
@@ -149,39 +163,91 @@ export default function Cart() {
                                         ))}
                                     </tbody>
                                 </table>
-                            </section>
+                            </section>}
+                            {windowInnerWidth < 991 && <section className="products w-100">
+                                <a href="#order-total" className="cart-total-btn-box">
+                                    Go To Cart Totals
+                                </a>
+                                {allProductsData.map((product, index) => (
+                                    <div className="product mb-4" key={index}>
+                                        <h4 className="mb-3">Product # {index + 1}</h4>
+                                        <table className="user-products-table-for-mobiles-and-tablets w-100">
+                                            <tbody>
+                                                <tr>
+                                                    <th>Product</th>
+                                                    <td className="product-cell">
+                                                        <img src={`${process.env.BASE_API_URL}/${product.imagePath}`} width="100" height="100" className="mb-3" />
+                                                        <h5 className="product-name mb-3">{product.name}</h5>
+                                                        <h6 className={`product-price ${product.discount != 0 ? "text-decoration-line-through" : ""}`}>{product.price} $</h6>
+                                                        {product.discount != 0 && <h6 className="product-after-discount">{product.price - product.discount} $</h6>}
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <th>Quantity</th>
+                                                    <td className="update-product-quantity-cell">
+                                                        <div className="update-product-quantity p-3 w-100">
+                                                            <HiMinus className="update-product-icon"
+                                                                onClick={() => updateProductQuantity(allProductsData, product.id, "decrease-product-quantity")}
+                                                            />
+                                                            <span className="ms-3 me-3">{product.quantity}</span>
+                                                            <HiPlus className="update-product-icon"
+                                                                onClick={() => updateProductQuantity(allProductsData, product.id, "increase-product-quantity")}
+                                                            />
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <th>Subtotal</th>
+                                                    <td className="subtotal-cell">
+                                                        {product.price * product.quantity} $
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <th>Process</th>
+                                                    <td className="delete-product-cell">
+                                                        <BsTrash className="trash-icon" onClick={() => deleteProduct(product.id)} />
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                ))}
+                            </section>}
                         </div>
-                        <div className="col-md-4">
-                            <section className="order-total border border-3 p-4 ps-5 pe-5 text-start">
+                        <div className="col-xl-4">
+                            <section className="order-total border border-3 p-4 ps-5 pe-5 text-start" id="order-total">
                                 <h5 className="fw-bold mb-5 text-center">Cart Totals</h5>
                                 <div className="row total-price-before-discount total pb-3 mb-5">
-                                    <div className="col-md-9 fw-bold text-start p-0">
+                                    <div className="col-md-9 fw-bold p-0">
                                         Total Price Before Discount
                                     </div>
-                                    <div className="col-md-3 fw-bold text-end p-0">
+                                    <div className="col-md-3 fw-bold p-0 text-md-end">
                                         {pricesDetailsSummary.totalPriceBeforeDiscount} $
                                     </div>
                                 </div>
                                 <div className="row total-price-discount total pb-3 mb-5">
-                                    <div className="col-md-9 fw-bold text-start p-0">
+                                    <div className="col-md-9 fw-bold p-0">
                                         Total Discount
                                     </div>
-                                    <div className="col-md-3 fw-bold text-end p-0">
+                                    <div className="col-md-3 fw-bold p-0 text-md-end">
                                         {pricesDetailsSummary.totalDiscount} $
                                     </div>
                                 </div>
                                 <div className="row total-price-after-discount total pb-3 mb-5">
-                                    <div className="col-md-9 fw-bold text-start p-0">
+                                    <div className="col-md-9 fw-bold p-0">
                                         Total Price After Discount
                                     </div>
-                                    <div className="col-md-3 fw-bold text-end p-0">
+                                    <div className="col-md-3 fw-bold p-0 text-md-end">
                                         {pricesDetailsSummary.totalPriceAfterDiscount} $
                                     </div>
                                 </div>
                                 <Link href="/checkout" className="checkout-link p-3 w-100 d-block text-center fw-bold">Go To Checkout</Link>
                             </section>
                         </div>
-                    </div> : <p className="alert alert-danger w-100 mx-auto">Sorry, Not Found Any Products !!</p>}
+                    </div> : <section className="not-found-any-products-for-user-in-cart text-center">
+                        <PiSmileySad className="sorry-icon mb-5" />
+                        <h1 className="h4">Sorry, Can't Find Any Products For You In Cart !!</h1>
+                    </section>}
                 </div>
             </div>
         </div>
