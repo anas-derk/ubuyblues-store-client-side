67aaff58acdf3ec87c89d5acd348fd1cf633cddd
anas derk <anas.derk2020@gmail.com>
2024-01-06 06:58:49 -0800

    Change Loader Page

--- src/components/LoaderPage/index.jsx
+++ src/components/LoaderPage/index.jsx
@@ -1,7 +1,9 @@
+import loaderImage from "../../../public/images/loaderImage.svg";
+
 export default function LoaderPage() {
     return (
         <div className="loading-box d-flex justify-content-center align-items-center">
-            <span className="loader-page"></span>
+            <img src={loaderImage.src} alt="Loader Image" />
         </div>
     );
 }
