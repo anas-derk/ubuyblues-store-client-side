6059e9cf97bc91753a124376856ae89df82dc104
anas derk <anas.derk2020@gmail.com>
2024-01-16 03:58:45 -0800

    Programming Dark Mode

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,16 +5,33 @@ import { BsFillPersonFill, BsPersonVcard } from "react-icons/bs";
 import { IoEarth } from "react-icons/io5";
 import { MdOutlineLogout } from "react-icons/md";
 import { useRouter } from "next/router";
+import { MdOutlineDarkMode, MdOutlineWbSunny } from "react-icons/md";
 
 export default function Header() {
 
     const [userId, setUserId] = useState("");
+
+    const [lightMode, setLightMode] = useState("sunny");
+
+    const handleChangeMode = () => {
+        const newLightMode = lightMode == "sunny" ? "dark" : "sunny";
+        setLightMode(newLightMode);
+        let rootElement = document.documentElement;
+        rootElement.style.setProperty("--main-color-one", newLightMode === "sunny" ? "#6A017A" : "#000");
+        localStorage.setItem("asfour-store-light-mode", newLightMode);
+    }
 
     const router = useRouter();
 
     useEffect(() => {
         const userId = localStorage.getItem("asfour-store-user-id");
         setUserId(userId);
+        const tempLightMode = localStorage.getItem("asfour-store-light-mode");
+        if (tempLightMode && (tempLightMode === "dark" || tempLightMode === "sunny")) {
+            setLightMode(tempLightMode);
+            let rootElement = document.documentElement;
+            rootElement.style.setProperty("--main-color-one", tempLightMode === "sunny" ? "#6A017A" : "#000");
+        }
     }, []);
 
     const userLogout = () => {
@@ -39,6 +56,15 @@ export default function Header() {
                     </Link>
                 </div>
                 <div className="languages-and-others-stores p-3">
+                    {lightMode == "sunny" ?
+                        <MdOutlineDarkMode
+                            className="dark-mode-icon global-header-icon me-5"
+                            onClick={handleChangeMode}
+                        /> :
+                        <MdOutlineWbSunny
+                            className="sunny-icon global-header-icon me-5"
+                            onClick={handleChangeMode}
+                        />}
                     <IoEarth className="earth-icon global-header-icon me-5" />
                     <Link href="/">
                         <AiOutlineHome className="home-icon global-header-icon" />
--- src/components/LoaderPage/index.jsx
+++ src/components/LoaderPage/index.jsx
@@ -1,6 +1,16 @@
+import { useEffect } from "react";
 import loaderImage from "../../../public/images/loaderImage.svg";
 
 export default function LoaderPage() {
+
+    useEffect(() => {
+        const tempLightMode = localStorage.getItem("asfour-store-light-mode");
+        if (tempLightMode && (tempLightMode === "dark" || tempLightMode === "sunny")) {
+            let rootElement = document.documentElement;
+            rootElement.style.setProperty("--main-color-one", tempLightMode === "sunny" ? "#6A017A" : "#000");
+        }
+    }, []);
+
     return (
         <div className="loading-box d-flex justify-content-center align-items-center">
             <img src={loaderImage.src} alt="Loader Image" />
