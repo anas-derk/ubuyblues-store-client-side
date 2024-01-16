7bb58d345061dcfac131956d167230c044f292df
anas derk <anas.derk2020@gmail.com>
2024-01-16 06:58:59 -0800

    Continue

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
+import { randomBytes } from 'crypto';
+
 export default NextAuth({
     providers: [
         Google({
@@ -8,4 +10,5 @@ export default NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         }),
     ],
+    secret: randomBytes(32).toString('hex'),
 });
