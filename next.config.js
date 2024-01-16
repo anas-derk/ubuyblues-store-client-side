3c2d4b5a4388dce1dca422ce8395f2367d14ce3c
anas derk <anas.derk2020@gmail.com>
2024-01-16 14:59:49 -0800

    Continue

--- next.config.js
+++ next.config.js
@@ -3,10 +3,7 @@ const nextConfig = {
   reactStrictMode: false,
   env: {
     BASE_API_URL: process.env.NODE_ENV === "development" ? "http://localhost:5200" : "https://api.ubuyblues.com",
-    WEBSITE_URL: process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://api.ubuyblues.com",
-    GOOGLE_CLIENT_ID: "817318940780-8drpve7i3kcado5llc1t86cbi3d1493s.apps.googleusercontent.com",
-    GOOGLE_CLIENT_SECRET: "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",
-    NEXTAUTH_URL: process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://ubuyblues.com",
+    WEBSITE_URL: process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://ubuyblues.com",
   },
   async headers() {
     return [
