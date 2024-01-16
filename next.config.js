2d1fafee3971b3fffc3d1562c2a113e989337886
anas derk <anas.derk2020@gmail.com>
2024-01-16 07:54:00 -0800

    Continue

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
     WEBSITE_URL: process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://api.ubuyblues.com",
     GOOGLE_CLIENT_ID: "300211308964-97saflbd4i2ra8sjki4ambufvf7f4s17.apps.googleusercontent.com",
     GOOGLE_CLIENT_SECRET: "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",
+    NEXTAUTH_URL: process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://ubuyblues.com",
   },
   async headers() {
     return [
