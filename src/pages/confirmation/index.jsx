547b89e2f5255af6ecbed15cb2a12fe9377cf8c8
anas derk <anas.derk2020@gmail.com>
2024-01-20 12:04:28 -0800

    Continue

--- src/pages/confirmation/index.jsx
+++ src/pages/confirmation/index.jsx
@@ -6,6 +6,7 @@ import axios from "axios";
 import ErrorOnLoadingThePage from "@/components/ErrorOnLoadingThePage";
 import ubuybluesLogo from "../../../public/images/UbuyBlues_Logo_merged_Purple.webp";
 import { FaRegSmileWink } from "react-icons/fa";
+import { useTranslation } from "react-i18next";
 
 export default function Confirmation({ orderId }) {
 
@@ -20,7 +21,11 @@ export default function Confirmation({ orderId }) {
         totalDiscount: 0,
     });
 
+    const { t, i18n } = useTranslation();
+
     useEffect(() => {
+        const userLanguage = localStorage.getItem("asfour-store-language");
+        handleSelectUserLanguage(userLanguage === "ar" || userLanguage === "en" || userLanguage === "tr" || userLanguage === "de" ? userLanguage : "en");
         axios.get(`${process.env.BASE_API_URL}/orders/order-details/${orderId}`)
             .then((res) => {
                 const result = res.data;
@@ -36,6 +41,11 @@ export default function Confirmation({ orderId }) {
                 setIsErrorMsgOnLoadingThePage(true);
             });
     }, []);
+
+    const handleSelectUserLanguage = (userLanguage) => {
+        i18n.changeLanguage(userLanguage);
+        document.body.lang = userLanguage;
+    }
 
     const calcTotalOrderPriceBeforeDiscount = (allProductsData) => {
         let tempTotalPriceBeforeDiscount = 0;
@@ -61,73 +71,77 @@ export default function Confirmation({ orderId }) {
             {!isLoadingPage && <>
                 <Header />
                 <div className="page-content text-white p-4">
-                    <h1 className="welcome-msg text-center mb-5">Welcome To You In Payment Confirmation Page</h1>
+                    <h1 className="welcome-msg text-center mb-5">{t("Welcome To You In Payment Confirmation Page")}</h1>
                     <section className="order-total border border-3 p-4 ps-md-5 pe-md-5 text-center" id="order-total">
-                        <h5 className="fw-bold mb-4 text-center">Your Request</h5>
+                        <h5 className="fw-bold mb-4 text-center">{t("Your Request")}</h5>
                         <div className="order-id-and-number border border-white border-2 p-4 mb-5">
-                            <h5 className="mb-4 text-center">Order Id: { orderDetails._id }</h5>
-                            <h5 className="mb-0 text-center">Order Number: { orderDetails.orderNumber }</h5>
+                            <h5 className="mb-4 text-center">{t("Order Id")}: {orderDetails._id}</h5>
+                            <h5 className="mb-0 text-center">{t("Order Number")}: {orderDetails.orderNumber}</h5>
                         </div>
-                        <h5 className="mb-5 text-center border border-white border-2 p-4">Order Details</h5>
+                        <h5 className="mb-5 text-center border border-white border-2 p-4">{t("Order Details")}</h5>
                         <div className="row total pb-3 mb-5">
                             <div className="col-md-3 fw-bold p-0">
-                                Product Name And Quantity
+                                {t("Product Name And Quantity")}
                             </div>
                             <div className="col-md-3 fw-bold p-0">
-                                Unit Price Before Discount
+                                {t("Unit Price Before Discount")}
                             </div>
                             <div className="col-md-3 fw-bold p-0">
-                                Unit Discount
+                                {t("Unit Discount")}
                             </div>
                             <div className="col-md-3 fw-bold p-0">
-                                Sum
+                                {t("Sum")}
                             </div>
                         </div>
                         {orderDetails.order_products.map((product, productIndex) => (
                             <div className="row total pb-3 mb-5" key={productIndex}>
                                 <div className="col-md-3 fw-bold p-0">
-                                    ( {product.name} ) x {product.quantity}
+                                    {i18n.language !== "ar" ? <span>
+                                        ( {product.name} ) x {product.quantity}
+                                    </span> : <span>
+                                        ( {product.name} ) {product.quantity} x
+                                    </span>}
                                 </div>
                                 <div className="col-md-3 fw-bold p-0">
-                                    {product.unit_price} $
+                                    {product.unit_price} {t("KWD")}
                                 </div>
                                 <div className="col-md-3 fw-bold p-0">
-                                    {product.discount} $
+                                    {product.discount} {t("KWD")}
                                 </div>
                                 <div className="col-md-3 fw-bold p-0">
-                                    {(product.unit_price - product.discount) * product.quantity} $
+                                    {(product.unit_price - product.discount) * product.quantity} {t("KWD")}
                                 </div>
                             </div>
                         ))}
                         <div className="row total-price-before-discount total pb-3 mb-5">
                             <div className="col-md-3 fw-bold p-0">
-                                Total Price Before Discount
+                                {t("Total Price Before Discount")}
                             </div>
-                            <div className="col-md-9 fw-bold p-0 text-md-end">
-                                {pricesDetailsSummary.totalPriceBeforeDiscount} $
+                            <div className={`col-md-9 fw-bold p-0 ${i18n.language !== "ar" ? "text-md-end" : "text-md-start"}`}>
+                                {pricesDetailsSummary.totalPriceBeforeDiscount} {t("KWD")}
                             </div>
                         </div>
                         <div className="row total-price-discount total pb-3 mb-5">
                             <div className="col-md-3 fw-bold p-0">
-                                Total Discount
+                                {t("Total Discount")}
                             </div>
-                            <div className="col-md-9 fw-bold p-0 text-md-end">
-                                {pricesDetailsSummary.totalDiscount} $
+                            <div className={`col-md-9 fw-bold p-0 ${i18n.language !== "ar" ? "text-md-end" : "text-md-start"}`}>
+                                {pricesDetailsSummary.totalDiscount} {t("KWD")}
                             </div>
                         </div>
                         <div className="row total-price-after-discount total pb-3 mb-5">
                             <div className="col-md-3 fw-bold p-0">
-                                Total Price After Discount
+                                {t("Total Price After Discount")}
                             </div>
-                            <div className="col-md-9 fw-bold p-0 text-md-end">
-                                {orderDetails.order_amount} $
+                            <div className={`col-md-9 fw-bold p-0 ${i18n.language !== "ar" ? "text-md-end" : "text-md-start"}`}>
+                                {orderDetails.order_amount} {t("KWD")}
                             </div>
                         </div>
                         <div className="thanks-icon-box mb-4">
                             <FaRegSmileWink className="thanks-icon" style={{ fontSize: "70px" }} />
                         </div>
                         <h4 className="mb-4">
-                            Thanks For Purchase From Ubuyblues Store
+                            {t("Thanks For Purchase From Ubuyblues Store")}
                         </h4>
                         <img
                             src={ubuybluesLogo.src}
