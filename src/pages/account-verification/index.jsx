dec648209092f49c11e68cbe7be21f8a4faf70ee
anas derk <anas.derk2020@gmail.com>
2024-01-20 11:34:38 -0800

    Continue

--- src/pages/account-verification/index.jsx
+++ src/pages/account-verification/index.jsx
@@ -7,6 +7,7 @@ import { useRouter } from "next/router";
 import LoaderPage from "@/components/LoaderPage";
 import validations from "../../../public/global_functions/validations";
 import { MdOutlineErrorOutline } from "react-icons/md";
+import { useTranslation } from "react-i18next";
 
 export default function AccountVerification({ email }) {
 
@@ -32,11 +33,15 @@ export default function AccountVerification({ email }) {
 
     const router = useRouter();
 
+    const { t, i18n } = useTranslation();
+
     useEffect(() => {
+        const userLanguage = localStorage.getItem("asfour-store-language");
+        handleSelectUserLanguage(userLanguage === "ar" || userLanguage === "en" || userLanguage === "tr" || userLanguage === "de" ? userLanguage : "en");
         if (validations.isEmail(email)) {
             sendTheCodeToUserEmail()
                 .then(() => setIsLoadingPage(false))
-                .catch(() => {
+                .catch((err) => {
                     setIsLoadingPage(false);
                 });
         } else {
@@ -44,6 +49,11 @@ export default function AccountVerification({ email }) {
             setIsLoadingPage(false);
         }
     }, []);
+
+    const handleSelectUserLanguage = (userLanguage) => {
+        i18n.changeLanguage(userLanguage);
+        document.body.lang = userLanguage;
+    }
 
     const sendTheCodeToUserEmail = async () => {
         try {
@@ -160,32 +170,32 @@ export default function AccountVerification({ email }) {
                 <div className="page-content">
                     <div className="container-fluid pb-5">
                         {!errorMsgOnLoading ? <>
-                            <h1 className="welcome-msg border-bottom pb-3 mb-5 text-center text-white">Welcome To You In Account Verification Page</h1>
+                            <h1 className="welcome-msg border-bottom pb-3 mb-5 text-center text-white">{t("Welcome To You In Account Verification Page")}</h1>
                             <section className="verification p-4">
-                                {errorMsg && <p className="alert alert-danger">{errorMsg}</p>}
-                                {successMsg && <p className="alert alert-success">{successMsg}</p>}
+                                {errorMsg && <p className="alert alert-danger">{t(errorMsg)}</p>}
+                                {successMsg && <p className="alert alert-success">{t(successMsg)}</p>}
                                 {isWaitCheckingStatus && <div className="overlay d-flex align-items-center justify-content-center flex-column text-white">
                                     <span className="check-loader mb-4"></span>
-                                    <h6>Checking ....</h6>
+                                    <h6>{t("Checking")} ....</h6>
                                 </div>}
                                 <div className="row">
                                     <div className="col-md-5">
-                                        <h6 className="mb-3 fw-bold">You're almost done!</h6>
+                                        <h6 className="mb-3 fw-bold">{t("You're almost done!")}</h6>
                                     </div>
                                     <div className="col-md-7 text-xl-end">
                                         <h6 className="mb-3 fw-bold">
-                                            You can redial the message after
+                                            {t("You can redial the message after")}
                                         </h6>
                                         <h6 className="mb-3 fw-bold">{minutes} : {seconds}</h6>
                                     </div>
                                 </div>
                                 <h6 className="mb-3 fw-bold">
-                                    <span className="me-2">We sent a launch code to</span>
+                                    <span className="me-2">{t("We sent a launch code to")}</span>
                                     <span className="text-danger email-box">{email}</span>
                                 </h6>
                                 <h6 className="mb-3 fw-bold">
                                     <FaLongArrowAltRight className="me-2" />
-                                    <span className="text-danger fw-bold">Enter code *</span>
+                                    <span className="text-danger fw-bold">{t("Enter code Here")} *</span>
                                 </h6>
                                 <form className="code-write-form d-flex mb-4" onSubmit={checkAccountVerificationCode}>
                                     {
@@ -207,26 +217,26 @@ export default function AccountVerification({ email }) {
                                     <input type="submit" hidden />
                                 </form>
                                 <div className="email-sent-manager-box pb-3">
-                                    <span className="fw-bold">Didn't get your email? </span>
+                                    <span className="fw-bold">{t("Didn't get your email?")} </span>
                                     {!isWaitResendTheCode && !errorMsg && <button
                                         className="btn btn-danger me-2"
                                         onClick={sendTheCodeToUserEmail}
                                         disabled={seconds == 0 && minutes == 0 ? false : true}
                                     >
-                                        Resend The Code
+                                        {t("Resend The Code")}
                                     </button>}
                                     {isWaitResendTheCode && <button
                                         className="btn btn-danger me-2"
                                         disabled
                                     >
-                                        Resending The Code ...
+                                        {t("Resending The Code")} ...
                                     </button>}
                                 </div>
                             </section>
                         </> : <section className="error-msg-on-loading d-flex justify-content-center align-items-center flex-column">
                             <main className="error-box p-4 text-center">
                                 <MdOutlineErrorOutline className="error-icon mb-4" />
-                                <p className="error-msg">{errorMsgOnLoading}</p>
+                                <p className="error-msg">{t(errorMsgOnLoading)}</p>
                             </main>
                         </section>}
                     </div>
