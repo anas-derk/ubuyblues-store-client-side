b9824bb30a9cb6d8e02ab35529e33da8c5fbac07
anas derk <anas.derk2020@gmail.com>
2024-01-21 04:33:59 -0800

    Add Germany Language

--- config/i18n.js
+++ config/i18n.js
@@ -2,11 +2,13 @@ import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
 import arTranslation from "./ar.translation.json";
 import trTranslation from "./tr.translation.json";
+import deTranslation from "./de.translation.json";
 
 i18next.use(initReactI18next).init({
     resources: {
         ar: { translation: arTranslation },
         tr: { translation: trTranslation },
+        de: { translation: deTranslation },
     },
     lng: "en",
     fallbackLng: 'en',
