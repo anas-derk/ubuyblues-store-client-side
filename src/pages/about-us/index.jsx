13bc5268ba7bb534b9b677bf97aef7b27b424c1e
anas derk <anas.derk2020@gmail.com>
2024-02-03 09:48:06 -0800

    Continue

--- src/pages/about-us/index.jsx
+++ src/pages/about-us/index.jsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Header from "@/components/Header";
 import { useTranslation } from "react-i18next";
 import { useState, useEffect } from "react";
+import LoaderPage from "@/components/LoaderPage";
 
 export default function AboutUs() {
 
@@ -25,19 +26,21 @@ export default function AboutUs() {
             <Head>
                 <title>Ubuyblues Store - About Us</title>
             </Head>
-            <Header />
-            <div className="page-content text-white p-4">
-                <div className="container-fluid">
-                    <h1 className="welcome-msg mb-5 border-bottom border-2 pb-3 w-fit mx-auto">{t("About Us")}</h1>
-                    <div className="content">
-                        <p className="mb-4">{t("We are Asfour International, a leading company in the wholesale and retail trade industry. We provide high-quality products to our customers. We are committed to meeting your needs and providing the best solutions to ensure your satisfaction")} .</p>
-                        <p className="mb-4">{t("Our company stands out with a wide range of diverse products available at competitive prices. We strive to provide a broad selection of products to suit your various needs and budgets. We offer high-quality products in various fields such as crafts, electronics, fashion, food products, household tools, and more")} .</p>
-                        <p className="mb-4">{t("We consider our customers as partners in our success, and we are committed to providing high-quality customer service. We pay attention to the smallest details and strive to offer a satisfying shopping experience for our customers")} .</p>
-                        <p className="mb-4">{t("We are dedicated to achieving the highest levels of quality and satisfaction for our customers. We strive for innovation and continuous development to meet the changing market demands and to continually improve our operations")} .</p>
-                        <p className="mb-0">{t("In summary, we are Asfour International, a leading wholesale and retail trade company, offering high-quality products at competitive prices and excellent customer service. We are committed to meeting your needs and achieving your satisfaction, striving for sustainable success in our business")} .</p>
+            {!isLoadingPage ? <>
+                <Header />
+                <div className="page-content text-white p-4">
+                    <div className="container-fluid">
+                        <h1 className="welcome-msg mb-5 border-bottom border-2 pb-3 w-fit mx-auto">{t("About Us")}</h1>
+                        <div className="content">
+                            <p className="mb-4">{t("We are Asfour International, a leading company in the wholesale and retail trade industry. We provide high-quality products to our customers. We are committed to meeting your needs and providing the best solutions to ensure your satisfaction")} .</p>
+                            <p className="mb-4">{t("Our company stands out with a wide range of diverse products available at competitive prices. We strive to provide a broad selection of products to suit your various needs and budgets. We offer high-quality products in various fields such as crafts, electronics, fashion, food products, household tools, and more")} .</p>
+                            <p className="mb-4">{t("We consider our customers as partners in our success, and we are committed to providing high-quality customer service. We pay attention to the smallest details and strive to offer a satisfying shopping experience for our customers")} .</p>
+                            <p className="mb-4">{t("We are dedicated to achieving the highest levels of quality and satisfaction for our customers. We strive for innovation and continuous development to meet the changing market demands and to continually improve our operations")} .</p>
+                            <p className="mb-0">{t("In summary, we are Asfour International, a leading wholesale and retail trade company, offering high-quality products at competitive prices and excellent customer service. We are committed to meeting your needs and achieving your satisfaction, striving for sustainable success in our business")} .</p>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </> : <LoaderPage />}
         </div>
     );
 }
--- src/pages/polices-terms-and-conditions/index.jsx
+++ src/pages/polices-terms-and-conditions/index.jsx
@@ -1,21 +1,105 @@
 import Head from "next/head";
 import Header from "@/components/Header";
+import { useTranslation } from "react-i18next";
+import { useState, useEffect } from "react";
+import LoaderPage from "@/components/LoaderPage";
 
 export default function PolicesTermsAndConditions() {
+
+    const [isLoadingPage, setIsLoadingPage] = useState(true);
+
+    const { t, i18n } = useTranslation();
+
+    useEffect(() => {
+        const userLanguage = localStorage.getItem("asfour-store-language");
+        handleSelectUserLanguage(userLanguage === "ar" || userLanguage === "en" || userLanguage === "tr" || userLanguage === "de" ? userLanguage : "en");
+        setIsLoadingPage(false);
+    }, []);
+
+    const handleSelectUserLanguage = (userLanguage) => {
+        i18n.changeLanguage(userLanguage);
+        document.body.lang = userLanguage;
+    }
+
     return (
         <div className="privacy-policy-and-conditions">
             <Head>
                 <title>Ubuyblues Store - Polices Terms And Conditions</title>
             </Head>
-            <Header />
-            <div className="page-content text-white p-4">
-                <div className="container-fluid">
-                    <h1 className="welcome-msg mb-5 border-bottom border-2 pb-3 w-fit mx-auto">Polices Terms And Conditions Page</h1>
-                    <div className="content">
-                        
+            {!isLoadingPage ? <>
+                <Header />
+                <div className="page-content text-white p-4">
+                    <div className="container-fluid">
+                        <h1 className="welcome-msg mb-5 border-bottom border-2 pb-3 w-fit mx-auto">{t("Polices-Terms & Conditions")}</h1>
+                        <div className="content">
+                            <h2 className="fw-bold mb-4 h3">{t("Introduction")}</h2>
+                            <p className="mb-5">{t("This privacy notice specifies how the international company Asfour or its owner or operator of this website collects and processes your personal data for the promotion, execution, and provision of Asfour services. By visiting this website, providing your personal data, or accessing and purchasing Asfour services, you explicitly agree to comply with the terms and conditions of this privacy notice. If you do not agree, please refrain from using or accessing this website or any of Asfour services")} .</p>
+                            <h2 className="fw-bold mb-4 h3">{t("Definitions")}</h2>
+                            <p className="mb-4">{t("The term 'subsidiary company' refers to any individual or entity, another individual or entity that, directly or indirectly, through one or more intermediaries, controls or is controlled by or is under common control with this individual or entity")} .</p>
+                            <p className="mb-4">{t("This refers to the products and services offered by International Asfour online on any website and/or services and call centers, including, but not limited to, the services provided by International Asfour")} .</p>
+                            <p className="mb-4">{t("This refers to a group of companies consisting of International Asfour and all its subsidiaries and affiliates collaborating with it")} .</p>
+                            <p className="mb-4">{t("'The applicable laws' refer to any privacy law, personal data protection law, regulation, treaty, rule, ordinance, license, restriction, judicial or administrative order, law or public law, or any other declaration having legal effect in the State of Kuwait")}</p>
+                            <p className="mb-4">{t("'The data subject' or 'You' refers to a specific or identifiable living individual to whom the personal data is related and who uses this website or the services of International Asfour")} .</p>
+                            <p className="mb-4">{t("'Personal data' refers to any information related to the data subject who can be identified, directly or indirectly, particularly by reference to an identifier such as a name, identification number, location data, online identifier, or one or more factors specific to the physical, physiological, genetic, mental, economic, cultural, or social identity of the data subject.")}</p>
+                            <p className="mb-4">{t("'Privacy notice' means this set of terms and conditions that govern the processing of personal data")}</p>
+                            <p className="mb-4">{t("'The website' refers to the electronic website, whose main page is identified by an address, and any application for mobile devices or desktop computer application developed by International Asfour or on its behalf")} .</p>
+                            <p className="mb-4">{t("'It' refers to the International Asfour company for wholesale and retail trade")}</p>
+                            <p className="mb-4">{t("'It' refers to the products and services offered by International Asfour online on this website, or the services and call centers that are referenced in this privacy notic")} .</p>
+                            <h2 className="fw-bold mb-4 h3">{t("What personal data do we collect ?")}</h2>
+                            <p className="mb-4">{t("We collect your personal data to provide and continually improve the services of International Asfour. We gather various types of personal data")} .</p>
+                            <h2 className="fw-bold mb-4 h3">{t("The personal data provided by the data subject")}</h2>
+                            <p className="mb-4">{t("To provide a seamless customer experience, we receive and store your personal data related to the services of International Asfour, including, but not limited to")} .</p>
+                            <p className="mb-4">{t("The name, address, and phone numbers")} .</p>
+                            <p className="mb-4">{t("Payment information")} .</p>
+                            <p className="mb-4">{t("age")} .</p>
+                            <p className="mb-4">{t("personal description, and photographic image in your profile")} .</p>
+                            <p className="mb-4">{t("Identity-related information and documents, including civil ID numbers, national ID, and driver's license")} .</p>
+                            <h2 className="fw-bold mb-4 h3">{t("We automatically collect and store certain types of personal data about your use of International Asfour services. We use 'cookies' and other unique identifiers, obtaining specific types of personal data when your web browser or device accesses International Asfour services. We may collect automatic personal data from, including but not limited to")} .</h2>
+                            <p className="mb-4">{t("The Internet Protocol (IP) address used to connect your computer to the internet")} .</p>
+                            <p className="mb-4">{t("Login information, email address, and password")} .</p>
+                            <p className="mb-4">{t("The location of your device or computer")} .</p>
+                            <p className="mb-4">{t("Device metrics such as when the device is in use, application usage, connection data, any errors or failure conditions, version, and timezone settings")} .</p>
+                            <p className="mb-4">{t("Date of purchase")} .</p>
+                            <p className="mb-4">{t("Information from other sources")} .</p>
+                            <h2 className="fw-bold mb-4 h3">{t("We may receive personal data about you from third-party sources, including International Asfour and affiliates, such as delivery and address information updated by our transportation companies, which we use to correct our records and facilitate the next purchase delivery. However, we are not responsible for the privacy practices of third-party sites that we do not own, operate, or control. We may collect personal data from other sources, including but not limited to")} .</h2>
+                            <p className="mb-4">{t("Delivery information and updated address from third parties")} .</p>
+                            <p className="mb-4">{t("Search results and links, including paid listings")} .</p>
+                            <p className="mb-4">{t("Third-party applications or websites that allow us to collect, share, and/or use your personal data")} .</p>
+                            <p className="mb-4">{t("Third-party sources providing marketing or demographic information about you to enhance our ability to tailor our content and provide International Asfour services that we believe may interest you")} .</p>
+                            <h2 className="fw-bold mb-4 h3">{t("The purposes for which we use your personal data")} .</h2>
+                            <p className="mb-4">{t("International Asfour uses your personal data to operate, provide, develop, and improve the services of the International Asfour website and application, including but not limited to")} :</p>
+                            <h5 className="fw-bold mb-4">{t("Purchasing, delivery, and promotion")}</h5>
+                            <p className="mb-4">{t("International Asfour uses your personal data to receive and process orders, provide products and services, process payments, and communicate with you regarding orders, products, services, and promotional offers from International Asfour")}</p>
+                            <h5 className="fw-bold mb-4">{t("Providing error detection and correction features and improving the services of International Asfour")}</h5>
+                            <p className="mb-4">{t("International Asfour uses your personal data to provide operational functions, analyze performance, troubleshoot errors, and enhance the usability and effectiveness of International Asfour services")}</p>
+                            <h5 className="fw-bold mb-4">{t("Recommendations and customization")}</h5>
+                            <p className="mb-4">{t("International Asfour uses your personal data to recommend products and services that may interest you, identify your preferences, and personalize your experience with International Asfour")}</p>
+                            <h5 className="fw-bold mb-4">{t("Compliance with legal obligations")}</h5>
+                            <p className="mb-4">{t("In some cases, International Asfour collects your personal data and uses it to comply with applicable laws")}</p>
+                            <h5 className="fw-bold mb-4">{t("Advertisements")}</h5>
+                            <p className="mb-4">{t("International Asfour uses your personal data to display interest-based advertisements for features, products, and services that may be of interest to you. International Asfour does not use personally identifiable information to display interest-based ads")}</p>
+                            <h5 className="fw-bold mb-4">{t("Preventing fraud and credit risks")}</h5>
+                            <p className="mb-4">{t("International Asfour uses your personal data to prevent, detect, and investigate fraud and misuse in order to protect the security of International Asfour and its customers. International Asfour may also use scoring methods to assess and manage risks")}</p>
+                            <h2 className="fw-bold mb-4 h3">{t("Disclosure and sharing of your personal data")}</h2>
+                            <p className="mb-4">{t("We may share your personal data within International Asfour to provide you access to the services of the website and the application. International Asfour may market website and application services to you as a result of such sharing unless you explicitly unsubscribe")} .</p>
+                            <h2 className="fw-bold mb-4 h3">{t("Data subject rights")}</h2>
+                            <p className="mb-4">{t("When we process your personal data, you have several rights regarding how the personal data is processed, and you can exercise these rights at any time. We have provided an overview of those rights below along with what it entails for you. You can exercise your rights by submitting a data subject rights request form available on this website")} .</p>
+                            <h5 className="fw-bold mb-4">{t("Right to access information")}</h5>
+                            <p className="mb-4">{t("You have the right to obtain clear, transparent, and easily understandable information about how we use your personal data")}</p>
+                            <h5 className="fw-bold mb-4">{t("Right of access")}</h5>
+                            <p className="mb-4">{t("You have the right to obtain a copy of the personal data we hold about you and to verify that we process it legally in accordance with applicable laws")}</p>
+                            <h5 className="fw-bold mb-4">{t("Right to rectification")}</h5>
+                            <p className="fw-bold mb-4">{t("You have the right to correct any incomplete or inaccurate personal data we hold about you. We may need to verify the accuracy of any new personal data you provide us")}</p>
+                            <h5 className="fw-bold mb-4">{t("Right to erasure")}</h5>
+                            <p className="mb-4">{t("You have the right to request the deletion or removal of personal data when there is no compelling reason for us to continue processing it. However, we may not always be able to comply with a deletion request for specific legal reasons, which will be notified to you, if applicable, at the time of your request")}</p>
+                            <h5 className="fw-bold mb-4">{t("Right to object")}</h5>
+                            <p className="mb-4">{t("In case of non-compliance with this privacy notice or applicable laws for the website and application, you have the right to object to certain types of processing, including processing for direct marketing purposes (e.g., receiving emails from us to notify you or contact you with various potential opportunities)")}</p>
+                            <h5 className="fw-bold mb-4">{t("Right to restrict data processing")}</h5>
+                            <p className="mb-4">{t("You have the right to restrict the processing of your personal data in the following scenarios :")}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </> : <LoaderPage />}
         </div>
     );
 }
