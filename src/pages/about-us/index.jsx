f294721630d6ebdd7b341751e660aa8c8cedac3e
anas derk <anas.derk2020@gmail.com>
2024-02-03 06:12:52 -0800

    Continue

--- src/pages/about-us/index.jsx
+++ src/pages/about-us/index.jsx
@@ -34,7 +34,7 @@ export default function AboutUs() {
                         <p className="mb-4">{t("Our company stands out with a wide range of diverse products available at competitive prices. We strive to provide a broad selection of products to suit your various needs and budgets. We offer high-quality products in various fields such as crafts, electronics, fashion, food products, household tools, and more")} .</p>
                         <p className="mb-4">{t("We consider our customers as partners in our success, and we are committed to providing high-quality customer service. We pay attention to the smallest details and strive to offer a satisfying shopping experience for our customers")} .</p>
                         <p className="mb-4">{t("We are dedicated to achieving the highest levels of quality and satisfaction for our customers. We strive for innovation and continuous development to meet the changing market demands and to continually improve our operations")} .</p>
-                        <p className="mb-4">{t("In summary, we are Asfour International, a leading wholesale and retail trade company, offering high-quality products at competitive prices and excellent customer service. We are committed to meeting your needs and achieving your satisfaction, striving for sustainable success in our business")} .</p>
+                        <p>{t("In summary, we are Asfour International, a leading wholesale and retail trade company, offering high-quality products at competitive prices and excellent customer service. We are committed to meeting your needs and achieving your satisfaction, striving for sustainable success in our business")} .</p>
                     </div>
                 </div>
             </div>
