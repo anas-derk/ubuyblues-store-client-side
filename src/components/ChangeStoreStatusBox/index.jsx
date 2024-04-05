7015e611271ae21519717cc45391e2477aef7df0
anas derk <anas.derk2020@gmail.com>
2024-04-04 19:27:53 -0700

    Continue

--- src/components/ChangeStoreStatusBox/index.jsx
+++ src/components/ChangeStoreStatusBox/index.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { GrFormClose } from "react-icons/gr";
 import axios from "axios";
+import validations from "../../../public/global_functions/validations";
 
 export default function ChangeStoreStatusBox({
     setIsDisplayChangeStoreStatusBox,
@@ -18,30 +19,63 @@ export default function ChangeStoreStatusBox({
 
     const [errorMsg, setErrorMsg] = useState("");
 
+    const [formValidationErrors, setFormValidationErrors] = useState({});
+
     const handleClosePopupBox = () => {
         setIsDisplayChangeStoreStatusBox(false);
         setNewStoreStatus("");
     }
 
-    const changeStoreStatus = async (storeId, newStoreStatus) => {
-        try {
-            setIsWaitStatus(true);
-            const res = await axios.put(`${process.env.BASE_API_URL}/stores/update-store-status/${storeId}`,
-                newStoreStatus === "rejecting" || newStoreStatus === "blocking" ? {
-                    changeStatusReason,
-                } : {}, {
-                    headers: {
-                        Authorization: token,
-                    }
-                }
-            );
-            const result = res.data;
-            if (!result.error) {
+    const validateFormFields = (validateDetailsList) => {
+        return validations.inputValuesValidation(validateDetailsList);
+    }
 
+    const changeStoreStatus = async (e, storeId, newStoreStatus) => {
+        try {
+            e.preventDefault();
+            let errorsObject = {}, bodyData = {};
+            if (newStoreStatus === "rejecting" || newStoreStatus === "blocking") {
+                errorsObject = validateFormFields([
+                    {
+                        name: "changeStatusReason",
+                        value: changeStatusReason,
+                        rules: {
+                            isRequired: {
+                                msg: "Sorry, This Field Can't Be Empty !!",
+                            },
+                        },
+                    },
+                ]);
+                setFormValidationErrors(errorsObject);
+            }
+            console.log(errorsObject)
+            if (Object.keys(errorsObject).length == 0) {
+                setIsWaitStatus(true);
+                const res = await axios.put(`${process.env.BASE_API_URL}/stores/update-store-status/${storeId}`, bodyData,
+                    {
+                        headers: {
+                            Authorization: token,
+                        }
+                    }
+                );
+                const result = res.data;
+                if (!result.error) {
+
+                }
             }
         }
         catch (err) {
-
+            console.log(err)
+            if (err?.response?.data?.msg === "Unauthorized Error") {
+                await router.push("/admin-dashboard/login");
+                return;
+            }
+            setIsWaitStatus(false);
+            setErrorMsg("Sorry, Someting Went Wrong, Please Repeate The Process !!");
+            let errorTimeout = setTimeout(() => {
+                setErrorMsg("");
+                clearTimeout(errorTimeout);
+            }, 1500);
         }
     }
 
@@ -51,57 +85,88 @@ export default function ChangeStoreStatusBox({
                 {!isWaitStatus && !errorMsg && !successMsg && <GrFormClose className="close-popup-box-icon" onClick={handleClosePopupBox} />}
                 <h2 className="mb-5 pb-3 border-bottom border-white">Change Store Status</h2>
                 <h4 className="mb-4">Are You Sure From: {newStoreStatus} Store: ( {storeId} ) ?</h4>
-                {
-                    !isWaitStatus &&
-                    !errorMsg &&
-                    !successMsg &&
-                    newStoreStatus === "approving" &&
+                <form className="change-store-status-form w-50" onSubmit={(e) => changeStoreStatus(e, storeId, newStoreStatus)}>
+                    {(newStoreStatus === "rejecting" || newStoreStatus === "blocking") && <section className="change-store-status mb-4">
+                        <input
+                            type="text"
+                            className={`form-control p-3 border-2 change-status-reason-field ${formValidationErrors["changeStatusReason"] ? "border-danger mb-3" : "mb-4"}`}
+                            placeholder={`Please Enter ${newStoreStatus} Reason`}
+                            onChange={(e) => setChangeStatusReason(e.target.value)}
+                            value={changeStatusReason}
+                        />
+                        {formValidationErrors["changeStatusReason"] && <p className="bg-danger p-2 form-field-error-box m-0 text-white">
+                            <span className="me-2"><HiOutlineBellAlert className="alert-icon" /></span>
+                            <span>{formValidationErrors["changeStatusReason"]}</span>
+                        </p>}
+                    </section>}
+                    {
+                        !isWaitStatus &&
+                        !errorMsg &&
+                        !successMsg &&
+                        newStoreStatus === "approving" &&
+                        <button
+                            className="btn btn-success d-block mx-auto mb-4 global-button"
+                            type="submit"
+                        >
+                            Approve
+                        </button>
+                    }
+                    {
+                        !isWaitStatus &&
+                        !errorMsg &&
+                        !successMsg &&
+                        newStoreStatus === "rejecting" &&
+                        <button
+                            className="btn btn-success d-block mx-auto mb-4 global-button"
+                            type="submit"
+                        >
+                            Reject
+                        </button>
+                    }
+                    {
+                        !isWaitStatus &&
+                        !errorMsg &&
+                        !successMsg &&
+                        newStoreStatus === "blocking" &&
+                        <button
+                            className="btn btn-success d-block mx-auto mb-4 global-button"
+                            type="submit"
+                        >
+                            Block
+                        </button>
+                    }
+                    {isWaitStatus &&
+                        <button
+                            className="btn btn-info d-block mx-auto mb-3 global-button"
+                            disabled
+                        >
+                            Please Waiting ...
+                        </button>
+                    }
+                    {errorMsg &&
+                        <button
+                            className="btn btn-danger d-block mx-auto mb-3 global-button"
+                            disabled
+                        >
+                            { errorMsg }
+                        </button>
+                    }
+                    {successMsg &&
+                        <button
+                            className="btn btn-success d-block mx-auto mb-3 global-button"
+                            disabled
+                        >
+                            { successMsg }
+                        </button>
+                    }
                     <button
-                        className="btn btn-success d-block mx-auto mb-4 global-button"
-                        onClick={() => changeStoreStatus(storeId, newStoreStatus)}
+                        className="btn btn-danger d-block mx-auto global-button"
+                        disabled={isWaitStatus || errorMsg || successMsg}
+                        onClick={handleClosePopupBox}
                     >
-                        Approve
+                        Close
                     </button>
-                }
-                {
-                    !isWaitStatus &&
-                    !errorMsg &&
-                    !successMsg &&
-                    newStoreStatus === "rejecting" &&
-                    <button
-                        className="btn btn-success d-block mx-auto mb-4 global-button"
-                        onClick={() => changeStoreStatus(storeId, newStoreStatus)}
-                    >
-                        Reject
-                    </button>
-                }
-                {
-                    !isWaitStatus &&
-                    !errorMsg &&
-                    !successMsg &&
-                    newStoreStatus === "blocking" &&
-                    <button
-                        className="btn btn-success d-block mx-auto mb-4 global-button"
-                        onClick={() => changeStoreStatus(storeId, newStoreStatus)}
-                    >
-                        Block
-                    </button>
-                }
-                {isWaitStatus &&
-                    <button
-                        className="btn btn-info d-block mx-auto mb-3 global-button"
-                        disabled
-                    >
-                        Please Waiting ...
-                    </button>
-                }
-                <button
-                    className="btn btn-danger d-block mx-auto global-button"
-                    disabled={isWaitStatus || errorMsg || successMsg}
-                    onClick={handleClosePopupBox}
-                >
-                    Close
-                </button>
+                </form>
             </div>
         </div>
     );
