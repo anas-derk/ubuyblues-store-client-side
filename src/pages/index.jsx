9925adcee61e2ff1c7d3549ae63527baca33a2a4
anas derk <anas.derk2020@gmail.com>
2024-04-16 16:30:37 -0700

    Fix

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -185,6 +185,9 @@ export default function Home({ countryAsProperty, storeId }) {
                     }
                     setIsGetProducts(false);
                     // =============================================================================
+                } else {
+                    setIsGetCategories(false);
+                    setIsGetProducts(false);
                 }
             })
             .catch(() => {
