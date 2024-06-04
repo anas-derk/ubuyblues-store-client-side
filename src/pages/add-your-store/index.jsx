5ee50a5ab70d1189f78fb8ad89dee3365078b17f
anas derk <anas.derk2020@gmail.com>
2024-06-04 05:27:05 +0300

    Fix

--- src/pages/add-your-store/index.jsx
+++ src/pages/add-your-store/index.jsx
@@ -67,7 +67,7 @@ export default function AddYourStore() {
                     }
                 });
         } else {
-            setIsLoadingPage(false);
+            setIsGetUserInfo(false);
         }
     }, []);
 
