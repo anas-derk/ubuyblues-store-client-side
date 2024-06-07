d02e59e698b65cef45e0c4644a443ab4e9df5800
anas derk <anas.derk2020@gmail.com>
2024-06-07 03:41:38 +0300

    Continue

--- src/pages/checkout/index.jsx
+++ src/pages/checkout/index.jsx
@@ -154,7 +154,7 @@ export default function Checkout({ countryAsProperty, storeId }) {
                     });
                 }
             }
-            setIsGetUserInfo(true);
+            setIsGetUserInfo(false);
         }
         fetchData();
     }, []);
