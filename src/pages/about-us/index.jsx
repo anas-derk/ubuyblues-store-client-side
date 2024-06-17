1f3314c5dffff3b27c6ac466c1dda3e5920af05d
anas derk <anas.derk2020@gmail.com>
2024-06-17 20:51:01 +0300

    Re-Translate About Us Page

--- src/pages/about-us/index.jsx
+++ src/pages/about-us/index.jsx
@@ -51,15 +51,15 @@ export default function AboutUs() {
             </Head>
             {!isLoadingPage && !isErrorMsgOnLoadingThePage && <>
                 <Header />
-                <div className="page-content text-white p-4 page">
+                <div className="page-content text-white ps-4 pe-4 page">
                     <div className="container-fluid">
                         <h1 className="welcome-msg mb-5 border-bottom border-2 pb-3 w-fit mx-auto">{t("About Us")}</h1>
                         <div className="content">
-                            <p className="mb-4">{t("We are Asfour International, a leading company in the wholesale and retail trade industry. We provide high-quality products to our customers. We are committed to meeting your needs and providing the best solutions to ensure your satisfaction")} .</p>
-                            <p className="mb-4">{t("Our company stands out with a wide range of diverse products available at competitive prices. We strive to provide a broad selection of products to suit your various needs and budgets. We offer high-quality products in various fields such as crafts, electronics, fashion, food products, household tools, and more")} .</p>
-                            <p className="mb-4">{t("We consider our customers as partners in our success, and we are committed to providing high-quality customer service. We pay attention to the smallest details and strive to offer a satisfying shopping experience for our customers")} .</p>
-                            <p className="mb-4">{t("We are dedicated to achieving the highest levels of quality and satisfaction for our customers. We strive for innovation and continuous development to meet the changing market demands and to continually improve our operations")} .</p>
-                            <p className="mb-0">{t("In summary, we are Asfour International, a leading wholesale and retail trade company, offering high-quality products at competitive prices and excellent customer service. We are committed to meeting your needs and achieving your satisfaction, striving for sustainable success in our business")} .</p>
+                            <p className="mb-4">{t("We offer high-quality products to our customers. We are committed to meeting your needs and providing the best solutions to ensure your satisfaction .")} .</p>
+                            <p className="mb-4">{t("We feature a wide range of diverse products available at competitive prices. We strive to provide a variety of products to suit your different needs and budgets. We offer high-quality products in various fields such as handicrafts, home and office supplies, household products, and more .")} .</p>
+                            <p className="mb-4">{t("We consider our customers as partners in our success, and we strive to provide high-quality customer service. We pay attention to the smallest details and aim to provide a satisfying shopping experience for our customers .")} .</p>
+                            <p className="mb-4">{t("We are committed to dedicating ourselves to achieving the highest levels of quality and satisfaction for our customers. We strive for innovation and continuous development to meet the changing market demands and continually improve our operations .")} .</p>
+                            <p className="mb-0">{t("We are committed to meeting your needs and achieving your satisfaction, and we are dedicated to achieving sustainable success in our field of work .")} .</p>
                         </div>
                     </div>
                 </div>
