bc48dfb5c27187b3ebca79ee91e293073c400078
anas derk <anas.derk2020@gmail.com>
2024-06-22 14:35:44 +0300

    Test

--- src/components/SectionLoader/index.jsx
+++ src/components/SectionLoader/index.jsx
@@ -1,6 +1,6 @@
 export default function SectionLoader() {
     return (
-        <div className="section-loader d-flex flex-column align-items-center justify-content-center pt-5 pb-5">
+        <div className="section-loader d-flex flex-column align-items-center justify-content-center">
             <span className="loader-component"></span>
         </div>
     );
