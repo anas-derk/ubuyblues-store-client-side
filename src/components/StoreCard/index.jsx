ce692cda431aa1aaa58d8207956904b42835303c
anas-derk <anas.derk2023@gmail.com>
2025-03-02 23:55:05 +0300

    Fix

--- src/components/StoreCard/index.jsx
+++ src/components/StoreCard/index.jsx
@@ -8,8 +8,8 @@ export default function StoreCard({
     setSharingName,
     setSharingURL
 }) {
-    
-    const { t } = useTranslation();
+
+    const { i18n, t } = useTranslation();
 
     const handleDisplayShareOptionsBox = (sharingURL) => {
         setIsDisplayShareOptionsBox(true);
@@ -22,19 +22,19 @@ export default function StoreCard({
             <div
                 className="store-managment-box managment-box"
             >
-                <img src={`${process.env.BASE_API_URL}/${storeDetails.imagePath}`} alt={`${storeDetails.name} Store Image`} />
+                <img src={`${process.env.BASE_API_URL}/${storeDetails.imagePath}`} alt={`${storeDetails.name[i18n.language]} Store Image`} />
                 <Link className="store-overlay card-overlay" href={`/?storeId=${storeDetails._id}`}></Link>
                 <div className="store-managment-buttons managment-buttons p-2">
                     <PiShareFatLight
                         className="store-managment-icon managment-icon d-block"
-                        onClick={() => handleDisplayShareOptionsBox(`https://ubuyblues.com/?storeId=${storeDetails._id}`)}
+                        onClick={() => handleDisplayShareOptionsBox(`${process.env.WEBSITE_URL}/?storeId=${storeDetails._id}`)}
                     />
                 </div>
             </div>
             <div className="store-details details-box p-3 text-center">
-                <h6 className="store-name fw-bold mb-4 ">{t("Store Name")} : {storeDetails.name}</h6>
-                <h6 className="products-type fw-bold mb-4">{t("Products Type")} : {storeDetails.productsType}</h6>
-                <h6 className="products-description fw-bold">{t("Products Description")} : {storeDetails.productsDescription}</h6>
+                <h6 className="store-name fw-bold mb-4 ">{t("Store Name")} : {storeDetails.name[i18n.language]}</h6>
+                <h6 className="products-type fw-bold mb-4">{t("Products Type")} : {storeDetails.productsType[i18n.language]}</h6>
+                <h6 className="products-description fw-bold">{t("Products Description")} : {storeDetails.productsDescription[i18n.language]}</h6>
             </div>
         </div>
     );
