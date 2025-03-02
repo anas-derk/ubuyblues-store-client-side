41488b22be877707c76d2b8e6c7c7647b4e12a72
anas-derk <anas.derk2023@gmail.com>
2025-03-02 15:30:37 +0300

    Fix

--- src/components/BrandCard/index.jsx
+++ src/components/BrandCard/index.jsx
@@ -1,6 +1,10 @@
+import { useTranslation } from "react-i18next";
+
 export default function BrandCard({
     brandDetails,
 }) {
+
+    const { i18n } = useTranslation();
 
     return (
         <div className="brand-card card-box">
@@ -11,7 +15,7 @@ export default function BrandCard({
                 <div className="brand-overlay card-overlay"></div>
             </div>
             <div className="brand-details details-box p-3 text-center d-flex justify-content-center align-items-center">
-                <h6 className="brand-name fw-bold">{brandDetails.title}</h6>
+                <h6 className="brand-name fw-bold">{brandDetails.title[i18n.language]}</h6>
             </div>
         </div>
     );
