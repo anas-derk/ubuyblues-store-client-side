0a015d6fe3c261c5b5e7a3cde2e950f26a29b950
anas-derk <anas.derk2023@gmail.com>
2025-03-02 23:23:37 +0300

    Fix

--- src/components/SubCategoriesForParentSidBar/index.jsx
+++ src/components/SubCategoriesForParentSidBar/index.jsx
@@ -4,7 +4,7 @@ import { useTranslation } from "react-i18next";
 
 export default function SubCategoriesForParentSidBar({ subCategories }) {
 
-    const { i18n, t } = useTranslation();
+    const { i18n } = useTranslation();
 
     return (
         <aside className="sub-categories-for-parent-side-bar managment-links-side-bar bg-white">
@@ -18,7 +18,7 @@ export default function SubCategoriesForParentSidBar({ subCategories }) {
                             className="text-dark w-100 d-block managment-link fw-bold"
                         >
                             <RxDashboard className={`managment-link-icon ${i18n.language !== "ar" ? "me-3" : "ms-3"}`} />
-                            <span className="managment-link-name">{category.name}</span>
+                            <span className="managment-link-name">{category.name[i18n.language]}</span>
                         </Link>
                     </li>
                 ))}
